cd9e0836e36a27778a4ed145ddcfd0a5e013c732
teofiloxyz <105755958+teofiloxyz@users.noreply.github.com>
2024-04-17 19:50:15 +0100

    add main file

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from .src.business_auto_report import BusinessAutoReport
+
+
+def main() -> None:
+    BusinessAutoReport().generate_report()
+
+
+if __name__ == "__main__":
+    main()
