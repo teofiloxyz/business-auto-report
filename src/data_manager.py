dcc3fb0ef392f19cb168251bc593635574e24f8b
teofiloxyz <105755958+teofiloxyz@users.noreply.github.com>
2024-04-18 11:24:12 +0100

    add get first and latest ym methods

--- src/data_manager.py
+++ src/data_manager.py
@@ -35,4 +35,20 @@ class Database:
 
 
 class DataManager:
-    pass
+    def __init__(self) -> None:
+        self.db = Database()
+        self.date_utils = DateUtils()
+
+    def get_first_db_year_month(self) -> str:
+        query = f"""
+            SELECT strftime('%Y-%m', MIN(date)) AS year_month
+            FROM sales_fact
+        """
+        return self.db.fetch_result(query)[0]
+
+    def get_latest_db_year_month(self) -> str:
+        query = f"""
+            SELECT strftime('%Y-%m', MAX(date)) AS year_month
+            FROM sales_fact
+        """
+        return self.db.fetch_result(query)[0]
