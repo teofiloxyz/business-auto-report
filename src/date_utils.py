b35a7e513d188290572d1ac56c6a1feb3fa9319d
teofiloxyz <105755958+teofiloxyz@users.noreply.github.com>
2024-04-18 19:54:12 +0100

    add several DateUtils methods

--- src/date_utils.py
+++ src/date_utils.py
@@ -1,2 +1,19 @@
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+from typing import Tuple, List
+
+
 class DateUtils:
-    pass
+    def decompose_year_month(self, year_month: str) -> Tuple[int, int]:
+        year, month = map(int, year_month.split("-"))
+        return year, month
+
+    def get_previous_year_month(self, year_month: str) -> str:
+        current_ym = datetime.strptime(year_month, "%Y-%m")
+        previous_ym = current_ym - relativedelta(months=1)
+        return previous_ym.strftime("%Y-%m")
+
+    def get_next_year_month(self, year_month: str) -> str:
+        current_ym = datetime.strptime(year_month, "%Y-%m")
+        next_ym = current_ym + relativedelta(months=1)
+        return next_ym.strftime("%Y-%m")
