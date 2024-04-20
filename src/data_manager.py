c1d711e6afa154c8d6d0f0a9d342bdd8c6039954
teofiloxyz <105755958+teofiloxyz@users.noreply.github.com>
2024-04-20 19:08:41 +0100

    fix get_homologous_performance query

--- src/data_manager.py
+++ src/data_manager.py
@@ -79,16 +79,18 @@ class DataManager:
         # add expenses in the future
         year, month = self.date_utils.decompose_year_month(year_month)
         query = f"""
-            SELECT SUM(unit_price * quantity) / COUNT(DISTINCT date) AS average_daily_sales
+            SELECT strftime('%Y-%m', date) AS year_month,
+            SUM(unit_price * quantity) / COUNT(DISTINCT date) AS average_daily_sales
             FROM sales_fact
             LEFT JOIN products_dim ON sales_fact.product_id = products_dim.product_id
-            WHERE strftime('%Y-%m', date) >= '{year-3}-{month:02d}'
-            AND strftime('%Y-%m', date) <= '{year_month}'
-            AND strftime('%Y-%m', date) = '{month:02d}'
+            WHERE strftime('%Y', date) >= '{year-3}'
+            AND strftime('%Y', date) <= '{year}'
+            AND strftime('%m', date) = '{month:02d}'
+            GROUP by year_month
         """
         df = self.db.fetch_df_from_db(query)
-        previous = df.iloc[:-1].mean()
-        current = df.iloc[-1]
+        previous = df.iloc[:-1]["average_daily_sales"].mean()
+        current = df.iloc[-1]["average_daily_sales"]
         return (current - previous) / previous * 100
 
     def get_in_chain_performance(self, year_month: str) -> float:
